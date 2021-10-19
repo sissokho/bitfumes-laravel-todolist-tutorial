7804bd31d978527db32cd5d09d264aebefe0fb60
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-10-19 11:56:20 +0000

    Add 'create new todo' link

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,7 +1,11 @@
 @extends('todos.layout')
 
 @section('content')
-<h1 class="text-2xl">All your Todos</h1>
+<div class="flex justify-center mb-5">
+    <h1 class="text-2xl">All your Todos</h1>
+    <a href="/todos/create" class="mx-5 py-2 px-2 bg-blue-400 cursor-pointer rounded text-white">Create New</a>
+</div>
+
 <ul>
     @foreach ($todos as $todo)
     <li>{{ $todo->title }}</li>
