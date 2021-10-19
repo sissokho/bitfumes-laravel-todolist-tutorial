1ccebb1b57dbd7cbdded30825e8d1356abe820f6
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-10-19 13:39:01 +0000

    Add button to mark a todo as completed

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -9,9 +9,12 @@
 <ul class="pt-4">
     <x-alert />
     @foreach ($todos as $todo)
-    <li class="flex justify-between p-3">
-        <p>{{ $todo->title }}</p>
-        <a href="/todos/{{ $todo->id }}/edit" class="mx-5 py-2 px-2 bg-yellow-500 cursor-pointer rounded text-white">Edit</a>
+    <li class="flex justify-between items-end p-3">
+        <p class="{{ $todo->completed ? 'line-through' : '' }}">{{ $todo->title }}</p>
+        <div>
+            <a href="/todos/{{ $todo->id }}/edit" class="mx-5 py-2 px-2 bg-yellow-500 cursor-pointer rounded text-white">Edit</a>
+            <span class="px-2 text-4xl font-black {{ $todo->completed ? 'text-green-400' : 'text-gray-400' }} cursor-pointer">&check;</span>
+        </div>
     </li>
     @endforeach
 </ul>
